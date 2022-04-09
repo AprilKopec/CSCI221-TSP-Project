80daf472675bbc018a0815de4bcb3f89eddbc82a
April <89872609+AprilKopec@users.noreply.github.com>
2022-04-09 12:02:04 -0700

    Update cities.cpp

--- Part 1 (Random Search)/cities.cpp
+++ Part 1 (Random Search)/cities.cpp
@@ -17,6 +17,7 @@ double total_path_distance(const permutation_t& ordering) const {
 
 double total_path_distance() const {
 	distances = copy(city_vec);
-	// I would like to just for_each this but that won't handle the fact that we loop back to the start well
+	// I would like to just for_each this and then sum the resulting vector
+	// But we need to loop back to the start at the end
 	// So I'm not sure exactly what to do
 }
