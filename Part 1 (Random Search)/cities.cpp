5540e119204ce58bf70e12fd78a64ebf67e1b427
Aidan Gibson <aidangibson310@gmail.com>
2022-04-09 12:19:51 -0700

    misc fixes

--- Part 1 (Random Search)/cities.cpp
+++ Part 1 (Random Search)/cities.cpp
@@ -1,1 +1,10 @@
+#include <cmath>
 #include "cities.h"
+
+double Cities::total_path_distance(const Cities::permutation_t &ordering) const {
+    double dist = 0.0;
+    for (unsigned int i = 0; i<ordering.size();i++){
+        dist+=hypot(ordering[i],ordering[(i+1)%ordering.size()]);
+    }
+    return dist;
+}
